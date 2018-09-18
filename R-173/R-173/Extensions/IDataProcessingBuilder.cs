bfe4f80fae8e6e4fc816af989610da4dcf79e430
Timur <tgburh@gmail.com>
2018-09-18 13:30:03 +0300

    Improvement

---
+++ R-173/R-173/Extensions/IDataProcessingBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using P2PMulticastNetwork.Model;
+using RadioPipeline;
+
+namespace R_173.Extensions
+{
+    internal interface IDataProcessingBuilder
+    {
+        PipelineDelegate<DataModel> Build();
+
+        PipelineBuilder<DataModel> Use(Func<DataModel, PipelineDelegate<DataModel>, Task> action);
+    }
+
+    public class DataModelProcessingBuilder : PipelineBuilder<DataModel>, IDataProcessingBuilder { }
+}
