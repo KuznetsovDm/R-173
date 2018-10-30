784070b48a43db926f75532d971cc07a148f7efa
Timur <tgburh@gmail.com>
2018-10-30 15:19:58 +0300

    Steps features

--- R-173/R-173/BL/Steps.cs
+++ R-173/R-173/BL/Steps.cs
@@ -77,9 +77,9 @@ namespace R_173.BL
         }
     }
 
-    public class VolumeChangeState : Step
+    public class VolumeChangeStep : Step
     {
-        public VolumeChangeState(CheckState checkInputConditions = null, CheckState checkInternalState = null) : base(checkInputConditions, checkInternalState)
+        public VolumeChangeStep(CheckState checkInputConditions = null, CheckState checkInternalState = null) : base(checkInputConditions, checkInternalState)
         {
         }
 
@@ -90,15 +90,45 @@ namespace R_173.BL
         }
     }
 
-    public class NoiseChangedState : Step
+    public class NoiseChangedStep : Step
     {
-        public NoiseChangedState(CheckState checkInputConditions = null, CheckState checkInternalState = null) : base(checkInputConditions, checkInternalState)
+        public NoiseChangedStep(CheckState checkInputConditions = null, CheckState checkInternalState = null) : base(checkInputConditions, checkInternalState)
         {
         }
 
         protected override void Noise_ValueChanged(object sender, ValueChangedEventArgs<NoiseState> e)
         {
             if (e.NewValue == NoiseState.Minimum)
+            {
+                OnStepCompleted();
+            }
+        }
+    }
+
+    public class PrdPressStep : Step
+    {
+        public PrdPressStep(CheckState checkInputConditions = null, CheckState checkInternalState = null) : base(checkInputConditions, checkInternalState)
+        {
+        }
+
+        protected override void Sending_ValueChanged(object sender, ValueChangedEventArgs<SwitcherState> e)
+        {
+            if (e.NewValue == SwitcherState.Enabled)
+            {
+                OnStepCompleted();
+            }
+        }
+    }
+
+    public class PressToneStep : Step
+    {
+        public PressToneStep(CheckState checkInputConditions = null, CheckState checkInternalState = null) : base(checkInputConditions, checkInternalState)
+        {
+        }
+
+        protected override void Tone_ValueChanged(object sender, ValueChangedEventArgs<SwitcherState> e)
+        {
+            if (e.NewValue == SwitcherState.Enabled)
             {
                 OnStepCompleted();
             }
