db0cd23ef34a814a51e45f577cfb972bd56bf196
DK <tp5800@yandex.ru>
2019-02-26 13:50:33 +0300

    enter and esc behaviors

---
+++ R-173/R-173/SharedResources/MessageBehavior.cs
@@ -0,0 +1,68 @@
+﻿using R_173.Handlers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Unity;
+
+namespace R_173.SharedResources
+{
+    public class MessageBehavior
+    {
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.RegisterAttached(
+                "IsEnabled",
+                typeof(bool),
+                typeof(MessageBehavior),
+                new FrameworkPropertyMetadata(
+                    false, 
+                    FrameworkPropertyMetadataOptions.Inherits, 
+                    new PropertyChangedCallback(OnIsEnabledChanged)));
+
+
+        public static bool GetIsEnabled(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsEnabledProperty);
+        }
+
+        public static void SetIsEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsEnabledProperty, value);
+        }
+
+
+        private static void OnIsEnabledChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            if (obj is Button affirmativeButton && affirmativeButton.Name == "PART_AffirmativeButton")
+            {
+                var keyboardHandler = App.ServiceCollection.Resolve<KeyboardHandler>();
+                if (args.NewValue is true)
+                {
+                    keyboardHandler.OnEnterPressed = () =>
+                    {
+                        affirmativeButton.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+                    };
+                }
+                else
+                {
+                    keyboardHandler.OnEnterPressed = null;
+                }
+            }
+
+            if (obj is Button negativeButton && negativeButton.Name == "PART_NegativeButton")
+            {
+                var keyboardHandler = App.ServiceCollection.Resolve<KeyboardHandler>();
+                if (args.NewValue is true)
+                {
+                    keyboardHandler.OnEnterPressed = () =>
+                    {
+                        negativeButton.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+                    };
+                }
+                else
+                {
+                    keyboardHandler.OnEnterPressed = null;
+                }
+            }
+        }
+    }
+}
